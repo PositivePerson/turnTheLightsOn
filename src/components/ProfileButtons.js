3dcafe64ff87a124ccac806a7d7a53319c923d9f
Bartosz <WonderfulLifeVision@gmail.com>
2020-10-06 23:15:34 +0200

    functional buttons

--- src/components/ProfileButtons.js
+++ src/components/ProfileButtons.js
@@ -4,13 +4,17 @@ import React from 'react';
 const ProfileButtons = () => {
     return (
         <div className="d-flex">
-            <button className="btn waves-effect waves-light mx-2" style={constWidth} type="submit" name="action">Linkedin
+            <a href="https://www.linkedin.com/in/bartosz-galaszewicz/">
+                <button className="btn waves-effect waves-light mx-2" style={constWidth} name="action">Linkedin
             <i className="fab fa-linkedin" style={{ "marginLeft": "15px" }}></i>
-            </button>
-            <button className="btn waves-effect waves-light mx-2" style={constWidth} type="submit" name="action">Github
+                </button>
+            </a>
+            <a href="https://github.com/PositivePerson">
+                <button className="btn waves-effect waves-light mx-2" style={constWidth} name="action">Github
             <i className="fab fa-github-square" style={{ "marginLeft": "15px" }}></i>
-            </button>
-            <button className="btn waves-effect waves-light mx-2 disabled" style={constWidth} type="submit" name="action">Portfolio
+                </button>
+            </a>
+            <button className="btn waves-effect waves-light mx-2 disabled" style={constWidth} name="action">Portfolio
             <i className="fab fa-wpforms" style={{ "marginLeft": "15px" }}></i>
             </button>
         </div>
